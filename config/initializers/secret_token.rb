5a3a64f2a732e421544911a1ca8f4e9a6b128597
Brian Zeligson <brian.zeligson@gmail.com>
2013-10-25 09:13:40 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gradr::Application.config.secret_key_base = '2ef938637755e7f8a4a5e0fa757fc47f7fa3efd5a2dc6731000c4477558ce90a4807a6f05c10730d72a3994ca341ead9586af343ec2a6ce7d693d1625cd9fc59'
