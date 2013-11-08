95abbb2fda186170a831ea7709169a5596df0f39
Brian Zeligson <brian.zeligson@gmail.com>
2013-11-08 05:37:38 +0000

    add recent overall average to dashboard

--- app/models/concerns/gradee_stats.rb
+++ app/models/concerns/gradee_stats.rb
@@ -7,10 +7,20 @@ module GradeeStats
     (scores.sum.to_f / scores.size) rescue 0
   end
 
+  def scorecards_by_grader_since(grader_id, since=2.months.ago)
+     @scorecards_by_grader_since ||= {}
+     @scorecards_by_grader_since["#{grader_id}#{since}"] ||=
+      Scorecard.where('updated_at > ? and grader_id = ? and gradee_id = ?', since, grader_id, self.id)
+            .order('updated_at ASC').preload(:scores)
+  end
+
   def averages_by_grader_since(grader_id, since=2.months.ago)
-    Scorecard.where('updated_at > ? and grader_id = ? and gradee_id = ?', since, grader_id, self.id)
-                          .order('updated_at ASC').preload(:scores)
-                          .collect{|s| s.average_score }
+     self.scorecards_by_grader_since(grader_id, since).collect{|s| s.average_score }
+  end
+
+  def average_by_grader_since(grader_id, since=2.months.ago)
+    averages = self.averages_by_grader_since grader_id, since
+    (averages.sum.to_f / averages.size).round(2) rescue 0
   end
 
 end
